0c7c4af616d9fb8e7cc0811b86df426a83812645
Geekparade <david_da_silva0hotmail.fr>
2018-03-02 16:22:52 +0100

    petite fonction JS

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,20 @@
-console.log (3+5);
-function addExpense(){
+$(document).ready(function(){
+				
+	// Launch function on click button id calculate			
+	$("#calculate").click(function(){
+		var spend = 0.0;
+  			
+  			// Recuparate values of input class expense
+	    	$(".expense").each(function(){
+			 	spend = parseFloat(spend) + parseFloat($(this).val());
+				$("#result").val(spend)
+				});
 
-}
+	    	// Recuparate values of input class income
+	    	$(".income").each(function(){
+			 	spend = parseFloat(spend) + parseFloat($(this).val());
+				$("#result").val(spend)
+				});	
+	});
+});		
 
-function addIncome(){
-	
-}
